60e379a7411fd11af6b1cf3935dc804ecba441c5
saulo775 <59702227+saulo775@users.noreply.github.com>
2022-04-27 11:42:44 -0300

    Feat - Configurando servidor

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+
+const PORT = 5001;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor rodando na porta ${PORT}`)
+})
